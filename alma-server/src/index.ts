522474c63f7b0ee607b0bdef5c98388f500279f0
Widlund, Emil <ewidlund@ea.com>
2022-09-03 18:36:37 +0200

    Implement server

---
+++ alma-server/src/index.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "Emil",
+      email: "hello@emilwidlund.com",
+      username: "emilwidlund",
+    },
+  });
+  console.log(user);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
