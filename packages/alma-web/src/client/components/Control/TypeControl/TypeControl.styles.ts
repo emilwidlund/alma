46283ee743dadb3f360706d1da86580dca252084
Widlund, Emil <ewidlund@ea.com>
2022-11-13 23:04:34 +0100

    fix

--- packages/alma-web/src/client/components/Control/TypeControl/TypeControl.styles.ts
+++ packages/alma-web/src/client/components/Control/TypeControl/TypeControl.styles.ts
@@ -17,7 +17,16 @@ export const typeControlInputStyles = css`
     text-transform: uppercase;
     line-height: 1;
     letter-spacing: 0.1em;
-    padding: 6px;
+    padding: 8px 10px;
+    border-radius: 6px;
+    border: 1px solid var(--border-color);
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    background: transparent;
+    background-image: url("data:image/svg+xml;utf8,<svg fill='white' height='24' viewBox='0 0 24 24' width='24' xmlns='http://www.w3.org/2000/svg'><path d='M7 10l5 5 5-5z'/><path d='M0 0h24v24H0z' fill='none'/></svg>");
+    background-repeat: no-repeat;
+    background-position-x: 100%;
+    background-position-y: 0px;
 
     &:focus {
         outline: none;
