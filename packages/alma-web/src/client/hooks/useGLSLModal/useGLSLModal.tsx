23e602edb228053ab6895f8321e02bc304a86c3c
Widlund, Emil <ewidlund@ea.com>
2022-11-24 13:21:48 +0100

    fix moodals

--- packages/alma-web/src/client/hooks/useGLSLModal/useGLSLModal.tsx
+++ packages/alma-web/src/client/hooks/useGLSLModal/useGLSLModal.tsx
@@ -1,22 +1,54 @@
 import { GLSLNode } from 'alma-webgl';
+import { noop } from 'lodash';
 import * as React from 'react';
-import { useCallback } from 'react';
 
 import { GLSL_EDITOR_MODAL_ID } from '../../constants/modals';
-import { GLSLModal } from '../../containers/GLSLModal/GLSLModal';
-import { useModal } from '../useModal/useModal';
+import { ModalContext } from '../../providers/ModalProvider/ModalProvider';
+import { Size } from '../../types';
+import { IGLSLModalContentProps } from './useGLSLModal.types';
 
-export const useGLSLModal = () => {
-    const modal = useModal();
+export const GLSLModalContent = ({ node }: IGLSLModalContentProps) => {
+    const [glsl, setGLSL] = React.useState('');
+    const modal = React.useContext(ModalContext);
 
-    const open = useCallback((node: GLSLNode) => {
-        modal.open({
-            id: GLSL_EDITOR_MODAL_ID,
-            title: 'Edit GLSL',
-            children: <GLSLModal node={node} />,
-            actions: []
+    const onChange = React.useCallback((e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setGLSL(e.target.value);
+
+        modal.update({
+            actions: [
+                {
+                    label: 'Save',
+                    onPress: () => {
+                        node.setGLSL(e.target.value);
+                        modal.close(GLSL_EDITOR_MODAL_ID);
+                    },
+                    size: Size.MD
+                }
+            ]
         });
     }, []);
+
+    return (
+        <>
+            <textarea value={glsl} onChange={onChange}></textarea>
+        </>
+    );
+};
+
+export const useGLSLModal = () => {
+    const modal = React.useContext(ModalContext);
+
+    const open = React.useCallback(
+        (node: GLSLNode) => {
+            modal.queue({
+                id: GLSL_EDITOR_MODAL_ID,
+                title: 'Edit GLSL',
+                children: <GLSLModalContent node={node} />,
+                actions: [{ label: 'Save', onPress: noop, size: Size.MD }]
+            });
+        },
+        [modal]
+    );
 
     return {
         open,
