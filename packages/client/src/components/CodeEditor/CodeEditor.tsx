5a990eb04c364c1620b618bee601238e58f82f3b
Emil Widlund <hello@emilwidlund.com>
2023-06-21 22:49:11 +0200

    Fix window issue

--- packages/client/src/components/CodeEditor/CodeEditor.tsx
+++ packages/client/src/components/CodeEditor/CodeEditor.tsx
@@ -1,12 +1,24 @@
 'use client';
 
+import { loader } from '@monaco-editor/react';
 import dynamic from 'next/dynamic';
+import { useEffect } from 'react';
 
 import { CodeEditorProps } from './CodeEditor.types';
+import { language } from './languages/glsl/glsl';
+import { alma } from './themes/alma';
+
 const MonacoEditor = dynamic(() => import('@monaco-editor/react'), { ssr: false });
-import './monaco';
 
 export const CodeEditor = ({ value, onChange }: CodeEditorProps) => {
+    useEffect(() => {
+        loader.init().then(monaco => {
+            monaco.languages.register({ id: 'glsl' });
+            monaco.languages.setMonarchTokensProvider('glsl', language);
+            monaco.editor.defineTheme('alma', alma as any);
+        });
+    }, []);
+
     return (
         <div className="flex flex-col h-full items-stretch px-4 py-8">
             <MonacoEditor
