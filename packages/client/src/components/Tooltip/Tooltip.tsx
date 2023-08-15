d1423141b194b37262472e6b66ced4e6e8a0398a
Emil Widlund <hello@emilwidlund.com>
2023-08-15 11:19:42 +0200

    Remove emotion from tooltip

--- packages/client/src/components/Tooltip/Tooltip.tsx
+++ packages/client/src/components/Tooltip/Tooltip.tsx
@@ -1,9 +1,66 @@
-import { cx } from '@emotion/css';
 import * as React from 'react';
+import clsx from 'clsx';
 
-import { tooltipWrapperStyles, tooltipNodeWrapperStyles } from './Tooltip.styles';
-import { ITooltipNodeProps, ITooltipProps } from './Tooltip.types';
+import { ITooltipNodeProps, ITooltipProps, TooltipPosition } from './Tooltip.types';
 import { useHover } from '../../hooks/useHover/useHover';
+import { CSSProperties } from 'react';
+
+const getPositionalStyles = (
+    position: TooltipPosition,
+    dimensions: { width: number; height: number },
+    offset: { x: number; y: number } = { x: 0, y: 0 }
+): CSSProperties => {
+    const generalOffset = -10;
+    const combinedOffset = { x: -offset.x + generalOffset, y: -offset.y + generalOffset };
+    const { x, y } = combinedOffset;
+    const [verticalOffset, horizontalOffset] = [
+        `calc(${y}px + -${dimensions.height}px)`,
+        `calc(${x}px + -${dimensions.width}px)`
+    ];
+
+    switch (position) {
+        case TooltipPosition.TOP:
+            return {
+                top: verticalOffset,
+                left: '50%',
+                transform: 'translate(-50%, -50%)'
+            };
+        case TooltipPosition.TOP_RIGHT:
+            return {
+                top: verticalOffset,
+                right: horizontalOffset
+            };
+        case TooltipPosition.RIGHT:
+            return {
+                right: horizontalOffset
+            };
+        case TooltipPosition.BOTTOM_RIGHT:
+            return {
+                right: horizontalOffset,
+                bottom: verticalOffset
+            };
+        case TooltipPosition.BOTTOM:
+            return {
+                bottom: verticalOffset,
+                left: '50%',
+                transform: 'translate(-50%, -50%)'
+            };
+        case TooltipPosition.BOTTOM_LEFT:
+            return {
+                bottom: verticalOffset,
+                left: horizontalOffset
+            };
+        case TooltipPosition.LEFT:
+            return {
+                left: horizontalOffset
+            };
+        case TooltipPosition.TOP_LEFT:
+            return {
+                left: horizontalOffset,
+                top: verticalOffset
+            };
+    }
+};
 
 const TooltipNode = ({ text, position, offset }: ITooltipNodeProps) => {
     const ref = React.useRef<HTMLSpanElement>(null);
@@ -16,7 +73,11 @@ const TooltipNode = ({ text, position, offset }: ITooltipNodeProps) => {
     }, []);
 
     return (
-        <span ref={ref} className={tooltipNodeWrapperStyles(position, dimensions, offset)}>
+        <span
+            ref={ref}
+            className="absolute flex flex-row py-1 px-2 rounded-md bg-neutral-300 text-xxs uppercase tracking-wider shadow-lg z-20 nowrap"
+            style={getPositionalStyles(position, dimensions, offset)}
+        >
             {text}
         </span>
     );
@@ -26,7 +87,7 @@ export const Tooltip = ({ children, className, ...tooltipNodeProps }: ITooltipPr
     const { onMouseEnter, onMouseLeave, isHovered } = useHover();
 
     return (
-        <div className={cx(tooltipWrapperStyles, className)} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+        <div className={clsx('flex', className)} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
             <div>{children}</div>
             {isHovered && <TooltipNode {...tooltipNodeProps} />}
         </div>
