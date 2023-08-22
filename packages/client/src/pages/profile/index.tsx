931d317f2762be22335dd3f08ec835f049801729
Emil Widlund <hello@emilwidlund.com>
2023-08-22 22:42:22 +0200

    Return session

--- packages/client/src/pages/profile/index.tsx
+++ packages/client/src/pages/profile/index.tsx
@@ -29,7 +29,7 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
             }
         };
     } else {
-        return { props: {} }
+        return { props: { session } }
     }
 };
 
