d8ed96f81eab57ef96ae787800a215e622ea89e7
Emil Widlund <hello@emilwidlund.com>
2023-08-24 10:13:34 +0200

    Add animation to Modals

--- packages/client/src/components/Modal/Modal.tsx
+++ packages/client/src/components/Modal/Modal.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {motion} from 'framer-motion';
 
 import { ModalProps } from './Modal.types';
 import { Button } from '../Button/Button';
@@ -22,16 +23,16 @@ export const Modal = ({ modal: { title, children, actions, id }, onClose }: Moda
 
     return (
         <Portal wrapperId={id}>
-            <div className="flex flex-col justify-center items-center fixed inset-0 z-50 overflow-hidden" style={{ backgroundColor: 'rgba(0, 0, 0, .2)' }}>
-                <div className="flex flex-col rounded-3xl text-sm shadow-xl bg-neutral-100" style={{ width: 600 }}>
+            <motion.div className="flex flex-col justify-center items-center fixed inset-0 z-50 overflow-hidden" initial={{ backgroundColor: 'rgba(0, 0, 0, 0)' }} animate={{ backgroundColor: 'rgba(0, 0, 0, .2)' }}>
+                <motion.div className="flex flex-col rounded-3xl text-sm shadow-xl bg-neutral-100" style={{ width: 600 }} initial={{ opacity: 0, transform: 'translateY(-50px)' }} animate={{ opacity: 1, transform: 'translateY(0)' }}>
                     <div className="relative flex flex-col p-14 max-h-96 overflow-y-auto">{children}</div>
                     <div className="flex flex-row justify-center items-center py-8 px-12 border-t border-neutral-500 [&>*]:mr-4 [&>*:last-child]:mr-0">
                         {actions.map((action, index) => (
                             <Button key={index} {...action} />
                         ))}
                     </div>
-                </div>
-            </div>
+                </motion.div>
+            </motion.div>
         </Portal>
     );
 };
